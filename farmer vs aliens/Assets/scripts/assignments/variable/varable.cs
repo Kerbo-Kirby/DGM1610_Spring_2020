2850e3465f4ab183d387ea68a88c24d4f2419b59
kerbo-kirby <10740520@my.uvu.edu>
2020-01-15 17:20:21 -0700

    yea

---
+++ farmer vs aliens/Assets/scripts/assignments/variable/varable.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class varable : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    // Declaration
+    public float amount;
+    public float money;
+    public float cost;
+
+    // start is called before the first frame update
+
+    void Start()
+    {
+        Cookies( amount ,money , cost);
+
+
+
+    }
+ 
+    // Update is called once per frame
+    void Update()
+
+    {
+        
+    }
+
+     void Cookies(float amount, float money, float cost);
+    {
+    float total;
+        total = amount * cost - money;
+        debug.log (total);
+    }
+
+}
---
+++ farmer vs aliens/Assets/scripts/assignments/variable/variable.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class variable : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    // Declaration
+
+    public int cookieBox;
+
+    public float waterBottle;
+
+    private string firstName;
+
+    public double rbi;
+
+    public GameObject player;
+
+
+
+
+
+    // start is called before the first frame update
+
+    void Start()
+    {
+        cookieBox = 20;
+        firstName = "bobby";
+        Debug.Log("how man y cookeis are in the cox" + cookieBox);
+    }
+
+
+}
+
+// Update is called once per frame
+void Update()
+{
+
+}
+}
