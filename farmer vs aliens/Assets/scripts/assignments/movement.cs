b0064fc3dcfe7afbe8ecf014e7922a329f54a3b2
peyton <10740520@my.uvu.edu>
2020-01-27 17:15:03 -0700

    new farmers vs aliens project

---
+++ farmer vs aliens/Assets/scripts/assignments/movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, .5f);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+
+    }
+
+}
---
+++ project/Assets/Scenes/New Folder/New Folder/move.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, .05f);
+    }
+
+
+    void oncollisonenter(Collision other)
+    {
+
+
+        if (other.gameObject.CompareTag("floor"))
+
+        {
+            Debug.Log("collinding with floor");
+
+        }
+
+        else if (other.gameObject.CompareTag("obstacle"))
+        {
+            Debug.Log("collinding with obstacle");
+        }
+
+        else
+        {
+            Debug.Log("...");
+
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("you hace entered the trigger");
+    }
+}
