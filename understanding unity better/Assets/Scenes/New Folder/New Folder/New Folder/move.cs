7c3ad7d03b4a6c801fd8f472d038c366f20d933f
peyton kerbo <10740520@my.uvu.edu>
2020-01-30 14:25:54 -0700

    trying my best

---
+++ understanding unity better/Assets/Scenes/New Folder/New Folder/New Folder/move.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ove : MonoBehaviour
+{
+    /// <summary>
+    /// trying to figure out keys https://docs.unity3d.com/ScriptReference/KeyCode.html
+    /// </summary> and from what i learn in class time!!!
+
+
+
+
+    public float speed;
+    public float turnspeed;
+    public float verticalinput;
+    public float horizontalinput;
+
+
+
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        verticalinput = Input.GetAxis("vertical") ;
+
+
+        Input.GetKey(KeyCode.A);
+
+        
+
+        transform.Translate(Vector3.up);
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
---
+++ understanding unity better/Assets/Scenes/New Folder/New Folder/New Folder/textmesh.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class textmesh : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    /// <summary>
+    /// firguring out text generator information provited by unity forum 
+
+    /// </summary>
+    /// 
+
+    public Font font;
+
+
+
+
+
+    void Start()
+    {
+        TextGenerationSettings setting = new TextGenerationSettings();
+
+        // color of the text
+        setting.color = Color.black;
+
+        // where it will be placed
+        setting.generationExtents = new Vector2(5, 2);
+
+        setting.pivot = Vector2.zero;
+
+        // not sure what this means will study later
+        setting.richText = true;
+
+        setting.font = font;
+
+        //how big the font will be
+        setting.fontSize = 12;
+
+        //the font style that it will be
+        setting.fontStyle = FontStyle.Bold;
+
+
+        //text reaching a certain boundary vertically
+        setting.verticalOverflow = VerticalWrapMode.Overflow;
+
+        TextGenerator generator = new TextGenerator();
+
+        generator.Populate("im trying my best to learn by examples and see what happens",setting);
+
+        Debug.Log("I tried");
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
