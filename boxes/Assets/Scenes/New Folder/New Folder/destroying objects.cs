60675990a195bff1d332c338f73d22e238d897a5
peyton kerbo <10740520@my.uvu.edu>
2020-01-31 16:12:14 -0700

    boxes vs spheres
    trying to learn about sound effect through unity as well as destroying objects

---
+++ boxes/Assets/Scenes/New Folder/New Folder/destroying objects.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyingobjects : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+
+    //learned from  
+    void Start()
+    {
+        if (Input.GetKey(KeyCode.Space))
+
+        {
+
+            Destroy(obj);
+
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
