af54f7a7638dff37c8e4a88074f7cc756e29efa3
peyton kerbo <10740520@my.uvu.edu>
2020-02-04 20:05:15 -0700

    boxes
    points

---
+++ boxes/Assets/Scenes/scripts/assignments/pickup.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour
+{
+
+    public int pointsToAdd;
+    
+    
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+
+
+
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+        void onTriggerEnter(Collider other)
+        {
+
+if (other.gameObject.CompareTag("player"))
+            {
+                scoremanager.Addpoints(pointsToAdd);
+                Destroy(gameObject);
+
+
+            }
+
+
+        }
+
+
+    }
+
