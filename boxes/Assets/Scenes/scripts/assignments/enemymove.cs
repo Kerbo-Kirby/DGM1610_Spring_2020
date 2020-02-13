b71760b508b9bcc93582dc84421e1645b98ae1a7
peyton <10740520@my,uvu.edu>
2020-02-12 17:19:55 -0700

    spawn and enemy

---
+++ boxes/Assets/Scenes/scripts/assignments/enemymove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemymove : MonoBehaviour
+
+
+
+   
+{
+
+    public Transform target;
+    public float movespeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+        transform.Translate(Vector3.forward* movespeed *Time.deltaTime);
+    }
+}
---
+++ boxes/Assets/Scenes/scripts/assignments/spawnmanager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnmanager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject[] enemPrefabs;
+
+
+    public int enemyIndex;
+
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+
+            Instantiate(enemPrefabs[enemyIndex], new Vector3(0, 0, 0), enemPrefabs[enemyIndex].transform.rotation);
+        }
+    }
+}
