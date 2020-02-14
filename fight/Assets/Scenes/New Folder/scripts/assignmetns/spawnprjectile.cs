dd18996e4fd30f7dabb829b64d7e0c09e94cc604
peyton <10740520@my.uvu.edu>
2020-02-14 13:00:05 -0700

    fight
    messed with bullet scripts

---
+++ fight/Assets/Scenes/New Folder/scripts/assignmetns/spawnprjectile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnprjectile : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+
+    public GameObject bulletPrefab;
+
+
+    public GameObject spawnPoint;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bulletPrefab, spawnPoint.transform.position, Quaternion.identity);
+
+        }
+    }
+}
