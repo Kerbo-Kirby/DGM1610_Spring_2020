3977ef22cdab78974f5785ec5cb5285073255cb8
peyton kerbo <10740520@my.uvu.edu>
2020-02-19 13:57:35 -0700

    first
    added material and added more move script

---
+++ first person/Assets/Scenes/New Folder/scripts/assignments/move.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+
+
+
+    public float speed;
+
+
+    public float yeet = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        transform.Translate(Vector3.forward* Time.deltaTime * speed);
+        Debug.Log("gogogogogogoi");
+
+        if(Input.GetKey(KeyCode.S))
+        transform.Translate(Vector3.back * Time.deltaTime * speed);
+        Debug.Log("backback");
+
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        Debug.Log("yeet");
+
+            if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        Debug.Log("left lfet");
+
+
+        if (Input.GetKey(KeyCode.Space))
+            transform.Translate(Vector3.up * Time.deltaTime * yeet);
+        Debug.Log("yyeeeeeeeeeeeeeeeeeeeeeetttttttttttttttttttttt");
+
+
+        
+
+
+
+    }
+}
