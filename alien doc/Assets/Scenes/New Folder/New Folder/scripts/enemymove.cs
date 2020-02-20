c76bc865270e6e644a1c85d1196bc0401490ade0
peyton <10740520@my.uvu.edu>
2020-02-19 17:08:19 -0700

    alien
    in class because my original doesnet work

--- alien doc/Assets/Scenes/New Folder/New Folder/scripts/enemymove.cs
+++ alien doc/Assets/Scenes/New Folder/New Folder/scripts/enemymove.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+
+
+    public float speed;
+    public GameObject target;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(target);
+        transform.Translate(Vector3.forward* Time.deltaTime * speed);
     }
 }
