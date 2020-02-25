16ccf2045e0e42f542dd9bc020c814b58b477fda
peyton <10740520@my.uvu.edu>
2020-02-24 17:10:52 -0700

    farmer time
    more move stufff

---
+++ farmer and alien time/Assets/Scenes/New Folder/New Folder/enemymove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemymove : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject player;
+   // public Transform target;
+
+    public float movespeed;
+
+    private Rigidbody rbenemy;
+    void Start()
+    {
+        player = GameObject.Find("player");
+        rbenemy = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.LookAt(target);
+        // transform.Translate(Vector3.forward * movespeed * Time.deltaTime);
+
+    }
+
+    private void FixedUpdate()
+    {
+        rbenemy.AddForce((player.transform.position - transform.position).normalized * movespeed);
+
+
+
+
+    }
+
+
+
+
+}
