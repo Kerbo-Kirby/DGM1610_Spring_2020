272a44112fcd899ac9dd788cd6fc882c5d664ec6
peyton <10740520@my.uvu.edu>
2020-02-26 17:19:43 -0700

    farm
    class

---
+++ farmer and alien time/Assets/Scenes/New Folder/New Folder/bullet.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+
+    public int damage = 1;
+
+
+    public int time = 5;
+
+
+
+
+
+
+    void Start()
+    {
+        StartCoroutine(destroybullet());
+    }
+
+
+     void OnCollisionEnter(Collision other)
+    {
+   if (other.gameObject.CompareTag("enemy"))
+
+        {
+            var hit = other.gameObject;
+            var health = hit.GetComponent<EnemyHealth>();
+
+            if(health != null)
+            {
+
+                health.TakeDamage(damage);
+                Debug.Log("stop hes already dead");
+            }
+
+
+        }
+    }
+
+
+
+    IEnumerator destroybullet()
+
+    {
+
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+
+    }
+ 
+    
+}
