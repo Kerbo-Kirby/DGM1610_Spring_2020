dfd9c3f04954e15942bd65e035e886a4aa6a0483
peyton kerbo <10740520@my.uvu.edu>
2020-03-05 19:28:49 -0700

    floor is lava
    playing with boundarys

---
+++ floor is lava/Assets/Scenes/scripts/assighment/floorKill.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floorKill : MonoBehaviour
+{
+    // https://docs.unity3d.com/ScriptReference/CharacterController-isGrounded.html
+    // anoehter i implemented into my farm game wanted to see what i could do here
+
+    public GameObject player;
+
+    public GameObject floor;
+    CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            Destroy(player);
+        }
+
+        
+    }
+}
---
+++ floor is lava/Assets/Scenes/scripts/assighment/move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+   public float MoveSpeed;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * Time.deltaTime * MoveSpeed);
+
+        else if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.left * Time.deltaTime * MoveSpeed);
+
+       
+    }
+}
+
