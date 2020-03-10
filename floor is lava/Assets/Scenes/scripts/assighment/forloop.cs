6c88e232e99312d8c1b87bcd5c8143677130b8d1
peyton kerbo <10740520@my.uvu.edu>
2020-03-09 21:14:23 -0600

    loop practice

---
+++ floor is lava/Assets/Scenes/scripts/assighment/forloop.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class forloop : MonoBehaviour
+{
+
+
+    int ball = 5;
+    // Start is called before the first frame update
+    void Start()
+
+    {
+        for (int i = 0; i < ball; i++)
+
+            Debug.Log("ball" + i);
+
+        
+
+           
+        
+    }
+}
+   
+        
+    
+
+
+
+
+
+    
+
---
+++ floor is lava/Assets/Scenes/scripts/assighment/whileLoop.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class whileLoop : MonoBehaviour
+{
+
+
+
+    int cakeEaten = 10;
+    void Start()
+    {
+
+        while (cakeEaten > 0)
+            Debug.Log("you ate a lot!!! im worried");
+        cakeEaten--;
+    }
+
+    
+}
