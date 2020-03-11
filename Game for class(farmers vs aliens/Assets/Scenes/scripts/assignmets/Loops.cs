bcee22347971bae9d4de81b8e35991e16c9ed9a7
peyton <10740520@my.uvu.edu>
2020-03-11 17:09:30 -0600

    loop
    class

---
+++ Game for class(farmers vs aliens/Assets/Scenes/scripts/assignmets/Loops.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour
+{
+    public int numenemi = 3;
+
+    public int cupsInSink = 10;
+    void Start()
+    {
+        // for(int i = 0; i < numenemi; i++)
+        {
+            // Debug.Log("creating enemy" + i);
+        }
+
+        // while(cupsInSink > 0)
+        {
+
+            //   Debug.Log("i washed a cuppy");
+            // cupsInSink--;
+        }
+
+
+
+        //  bool shouldContinue = false;
+
+        // do
+        {
+            //print("hellow world");
+
+        } //while (shouldContinue == true);
+
+        string[] strings = new string[3];
+
+        strings[0] = " first string";
+        strings[1] = "second string ";
+        strings[2] = " thrid string";
+
+        foreach (string item in strings)
+        {
+            print(item);
+
+
+        }
+        
+
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+  
+}
