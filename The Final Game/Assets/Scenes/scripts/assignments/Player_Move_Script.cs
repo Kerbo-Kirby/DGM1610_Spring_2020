0e1cbaaeb8e8e5338bc138c4afca58fadff24c40
peyton kerbo <10740520@my.uvu.edu>
2020-03-20 13:50:43 -0600

    final game
    clean up
    
    added deck from maya
    
    added player move script

---
+++ The Final Game/Assets/Scenes/scripts/assignments/Player_Move_Script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move_Script : MonoBehaviour
+{
+    public float MoveSpeed = 5;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * Time.deltaTime * MoveSpeed);
+
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate (Vector3.back * Time.deltaTime * MoveSpeed);
+
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.left * Time.deltaTime * MoveSpeed);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.right * Time.deltaTime * MoveSpeed);
+    }
+}
