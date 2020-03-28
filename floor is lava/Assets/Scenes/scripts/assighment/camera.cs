dfed8c3c370b9ff8492be2b30a3dca0a1f5317f7
peyton kerbo <10740520@my.uvu.edu>
2020-03-28 15:29:55 -0600

    lava

--- floor is lava/Assets/Scenes/scripts/assighment/camera.cs
+++ floor is lava/Assets/Scenes/scripts/assighment/camera.cs
@@ -14,15 +14,15 @@ public class camera : MonoBehaviour
     {
 
         if (Input.GetKey(KeyCode.A))
-            transform.Translate(Vector3.left* Time.deltaTime* MoveSpeed);
+            transform.Translate(Vector3.left * Time.deltaTime * MoveSpeed);
 
-
-     
-
-        else if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
             transform.Translate(Vector3.right* Time.deltaTime* MoveSpeed);
 
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.up * Time.deltaTime * MoveSpeed);
 
-
-}
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.down* Time.deltaTime * MoveSpeed);
+    }
 }
