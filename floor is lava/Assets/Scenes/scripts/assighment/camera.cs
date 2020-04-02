22d7afeda8358c987bddf8a4ee98b61a670a43b5
peyton kerbo <10740520@my.uvu.edu>
2020-04-02 14:27:01 -0600

    script
    cleaning up code add floor prefab

--- floor is lava/Assets/Scenes/scripts/assighment/camera.cs
+++ floor is lava/Assets/Scenes/scripts/assighment/camera.cs
@@ -7,22 +7,20 @@ public class camera : MonoBehaviour
 {
 
 
-    public float MoveSpeed; 
-
+    public float MoveSpeed;
+    public Transform player;
 
     void Update()
     {
+
+        transform.LookAt(player);
 
         if (Input.GetKey(KeyCode.A))
             transform.Translate(Vector3.left * Time.deltaTime * MoveSpeed);
 
         if (Input.GetKey(KeyCode.D))
-            transform.Translate(Vector3.right* Time.deltaTime* MoveSpeed);
+            transform.Translate(Vector3.right * Time.deltaTime * MoveSpeed);
 
-        if (Input.GetKey(KeyCode.W))
-            transform.Translate(Vector3.up * Time.deltaTime * MoveSpeed);
-
-        if (Input.GetKey(KeyCode.S))
-            transform.Translate(Vector3.down* Time.deltaTime * MoveSpeed);
+       
     }
 }
