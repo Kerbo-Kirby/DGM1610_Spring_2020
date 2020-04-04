5bbfbbb2e431304ef93fc6e2c591365de3adc4b5
peyton kerbo <10740520@my.uvu.edu>
2020-04-04 17:42:55 -0600

    added
    banana health pick up

---
+++ floor is lava/Assets/Scenes/scripts/assighment/PlayerHealth.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public class Health_Pickup : pickUp
+
+
+        //it calls the pick up sctript and here it sets the banana health and ounce the plaiyer pick it up they get 5 more health
+
+    {
+        public int healthAmt = 5;
+
+
+
+
+    }
+
+}
