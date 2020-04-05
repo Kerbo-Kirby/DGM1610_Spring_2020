b39c50673e6eadc5c9488306d327be3191ef74eb
peyton kerbo <10740520@my.uvu.edu>
2020-04-05 14:32:28 -0600

    added bullet

---
+++ floor is lava/Assets/Scenes/scripts/assighment/Bullet.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public int damage = 1;
+    public int time = 4;
+
+    void Start()
+    {
+        StartCoroutine(DestroyBullet());
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+
+
+        /// if the enemy is hit it will get the enemy health varavle  and if its hit it takes damage 
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            var hit = other.gameObject;
+            var health = hit.GetComponent<EnemyHealth>();
+
+            if (health != null)
+            {
+                health.TakeDamage(damage);
+                Debug.Log("Ouch, you hit me!");
+            }
+
+        }
+
+    }
+    /// ageter the set amouint of time which is 4 secounds the bullet is destroyed
+  
+    IEnumerator DestroyBullet()
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+    }
+
+
+}
