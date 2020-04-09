7b148d4cce6717b8f666785873cffaa9c86a1b49
peyton kerbo <10740520@my.uvu.edu>
2020-04-08 18:35:18 -0600

    GAME
    Class

---
+++ floor is lava/Assets/Scenes/scripts/assighment/testmyOjection.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testmyOjection : MonoBehaviour
+{
+
+
+    /// public SO data;
+
+    /// private GameObject model;
+
+
+
+    void Start()
+    {
+       /// model = data.model;
+
+
+///   var modelRend = model.GetComponent<Renderer>();
+///  modelRend.sharedMaterial.SetColor(" color", data.color);
+
+/// print(data.name);
+/// print(data.description);/////
+
+////
+        }
+
+}
+
