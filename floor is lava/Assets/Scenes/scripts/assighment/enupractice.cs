175cf756c7a8058e7b1713552be563400a1d7f71
peyton kerbo <10740520@my.uvu.edu>
2020-04-15 20:09:36 -0600

    game
    practice

---
+++ floor is lava/Assets/Scenes/scripts/assighment/enupractice.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enumerations : MonoBehaviour
+{
+    enum FoodMenu { salad, sandwhich };
+
+    FoodMenu allfood;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allfood = FoodMenu.salad;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (allfood == FoodMenu.sandwhich)
+        {
+            print("yum yum yum!");
+        }
+    }
+}
