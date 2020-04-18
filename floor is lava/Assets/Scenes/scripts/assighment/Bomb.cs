685a189629012f89ec28cb10346e8d4b3cc4a5b0
peyton kerbo <10740520@my.uvu.edu>
2020-04-17 23:34:29 -0600

    GAMNE
    TRYING TO MAKE A BOMB IN THE GAME

---
+++ floor is lava/Assets/Scenes/scripts/assighment/Bomb.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public Rigidbody projectile;
+
+    public Transform Bombspawn;
+    public int Speed;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    
+        if (Input.GetKey(KeyCode.R))
+        {
+
+            Rigidbody clone;
+
+            clone = (Rigidbody)Instantiate(projectile, Bombspawn.position, projectile.rotation);
+
+            clone.velocity = Bombspawn.TransformDirection(Vector3.forward * Speed);
+        }
+    }
+}
